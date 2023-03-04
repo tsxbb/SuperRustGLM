3aea9296905b5564cc11a7aa93cd184afa6dd84d
tsxbb <142214302+tsxbb@users.noreply.github.com>
2023-03-04 12:54:10 +0000

    Updated CSS.

--- src/api_operation.rs
+++ src/api_operation.rs
@@ -1,4 +1,7 @@
 use std::error::Error;
 use once_cell::sync::OnceCell;
 use std::io::BufRead;
-use serde_derive::{Deserialize, 
+use serde_derive::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Serialize, Deserialize, Debug
