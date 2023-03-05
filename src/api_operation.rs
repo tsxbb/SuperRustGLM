0ccd857f95a7d6c1c7c5955c494a4c20d9d4e40b
tsxbb <142214302+tsxbb@users.noreply.github.com>
2023-03-05 21:45:31 +0000

    zram: kill unused zram_get_num_devices()

--- src/api_operation.rs
+++ src/api_operation.rs
@@ -4,4 +4,9 @@ use std::io::BufRead;
 use serde_derive::{Deserialize, Serialize};
 use serde_json::Value;
 
-#[derive(Serialize, Deserialize, Debug
+#[derive(Serialize, Deserialize, Debug)]
+struct ChatApiConfig {
+    api_key: Option<String>,
+}
+
+#[
