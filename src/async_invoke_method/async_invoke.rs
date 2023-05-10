ce01616a4c2d0dc9cf211bf18d2c31ee40fa7774
tsxbb <142214302+tsxbb@users.noreply.github.com>
2023-05-10 09:24:56 +0000

    fix locales

--- src/async_invoke_method/async_invoke.rs
+++ src/async_invoke_method/async_invoke.rs
@@ -30,4 +30,7 @@ struct AiConfig {
 }
 
 async fn async_read_config(file_path: &str, glm: &str) -> Result<String, Box<dyn Error>> {
-    let file_content = tokio::fs::read_to_string(file_pa
+    let file_content = tokio::fs::read_to_string(file_path).await?;
+    let config: AiConfig = toml::from_str(&file_content)?;
+
+    let response = mat
