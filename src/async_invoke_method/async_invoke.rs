e0940513600ca3a01a60b4689db9eb381515b717
tsxbb <142214302+tsxbb@users.noreply.github.com>
2023-06-25 17:18:16 +0000

    Delete .Rapp.history

--- src/async_invoke_method/async_invoke.rs
+++ src/async_invoke_method/async_invoke.rs
@@ -99,4 +99,4 @@ impl AsyncInvokeModel {
 
     pub async fn async_request(token: String, input: String, glm_version:&str,user_config: String, default_url: String, check_url: String) -> Result<String, Box<dyn Error>> {
         let mut async_invoke_model = Self::new();
-        Self::async_invoke_request_metho
+        Self::async_invoke_request_method(&mut async_invoke_model, token.clone(), input.clone(), glm_version,user
