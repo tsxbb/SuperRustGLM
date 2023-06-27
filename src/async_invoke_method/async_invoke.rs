301d8bd63f65ec55304f81c07c5da07c248b94e9
tsxbb <142214302+tsxbb@users.noreply.github.com>
2023-06-27 08:22:28 +0000

    Updated to latest API version.

--- src/async_invoke_method/async_invoke.rs
+++ src/async_invoke_method/async_invoke.rs
@@ -99,4 +99,5 @@ impl AsyncInvokeModel {
 
     pub async fn async_request(token: String, input: String, glm_version:&str,user_config: String, default_url: String, check_url: String) -> Result<String, Box<dyn Error>> {
         let mut async_invoke_model = Self::new();
-        Self::async_invoke_request_method(&mut async_invoke_model, token.clone(), input.clone(), glm_version,user
+        Self::async_invoke_request_method(&mut async_invoke_model, token.clone(), input.clone(), glm_version,user_config.clone(), default_url.clone()).await?;
+        let search_id = a
