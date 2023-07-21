b69c48deb4f2cf75413d7affb6743b433dcd2cec
tsxbb <142214302+tsxbb@users.noreply.github.com>
2023-07-21 13:56:03 +0000

    Fixed up tests

--- src/async_invoke_method/async_invoke.rs
+++ src/async_invoke_method/async_invoke.rs
@@ -183,4 +183,7 @@ impl AsyncInvokeModel {
             .as_f64().expect("Failed to get max_token");
 
         let temp_float = json_value[0]["temp_float"]
-            .as_f64().expect("Failed to get t
+            .as_f64().expect("Failed to get temp_float");
+
+        let top_p_float = json_value[0]["top_p_float"]
+     
