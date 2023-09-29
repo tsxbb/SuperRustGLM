f711759eb300e2a33b066b046e2555452de58cac
tsxbb <142214302+tsxbb@users.noreply.github.com>
2023-09-29 13:05:30 +0000

    captions

--- src/async_invoke_method/async_invoke.rs
+++ src/async_invoke_method/async_invoke.rs
@@ -346,4 +346,5 @@ impl AsyncInvokeModel {
     }
 
     fn convert_unicode_emojis(&self, input: &str) -> String {
-        let regex = regex:
+        let regex = regex::Regex::new(r"\\u[0-9a-fA-F]{4}").unwrap();
+        let result = regex.replace_al
