5ac5b1bfd84837e600a76387cf50f4452aaed58c
tsxbb <142214302+tsxbb@users.noreply.github.com>
2023-10-03 11:34:02 +0000

    add share

--- src/async_invoke_method.rs
+++ src/async_invoke_method.rs
@@ -2,4 +2,7 @@ mod async_invoke;
 
 use crate::async_invoke_method::async_invoke::AsyncInvokeModel;
 
-pub struct ReceiveAsyncInvokeOnlyTe
+pub struct ReceiveAsyncInvokeOnlyText {
+    response_async_message: Option<String>,
+    default_url: String,
+    as
