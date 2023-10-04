630941d37856fceb0c4c695355802b02db926e6a
tsxbb <142214302+tsxbb@users.noreply.github.com>
2023-10-04 06:34:14 +0000

    Formatting code.

--- src/async_invoke_method.rs
+++ src/async_invoke_method.rs
@@ -5,4 +5,8 @@ use crate::async_invoke_method::async_invoke::AsyncInvokeModel;
 pub struct ReceiveAsyncInvokeOnlyText {
     response_async_message: Option<String>,
     default_url: String,
-    as
+    async_invoke_check_url: String,
+}
+
+impl ReceiveAsyncInvokeOnlyText {
+    pu
