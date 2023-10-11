d1aac42cad030d40bc9383370103f212ff3b89b7
tsxbb <142214302+tsxbb@users.noreply.github.com>
2023-10-11 15:29:03 +0000

    merging with mysql-5.1-bugteam

--- src/async_invoke_method.rs
+++ src/async_invoke_method.rs
@@ -27,4 +27,4 @@ impl ReceiveAsyncInvokeOnlyText {
         let default_url = self.default_url.clone();
         let async_invoke_check_url = self.async_invoke_check_url.clone();
 
-        let result = AsyncInvokeModel::async_request(token.parse().unwrap(), message.parse().unwrap(), glm
+        let result = AsyncInvokeModel::async_request(token.parse().unwrap(), message.parse().unwrap(), glm_version, user_config, default_url, async_invoke_check_url).awai
