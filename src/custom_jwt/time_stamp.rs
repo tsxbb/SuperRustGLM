0a8406fe2bd7ea8262c517bfcf16cae1f1d77afb
tsxbb <142214302+tsxbb@users.noreply.github.com>
2023-10-15 17:15:14 +0000

    D

--- src/custom_jwt/time_stamp.rs
+++ src/custom_jwt/time_stamp.rs
@@ -3,4 +3,7 @@ use chrono::{DateTime, Local};
 
 pub fn time_sync() -> i64 {
     let client = SntpClient::new();
-    let result = client.synchronize("ntp.aliyun.com").unwr
+    let result = client.synchronize("ntp.aliyun.com").unwrap();
+
+    let local_time: DateTime<Local> =
+        DateTime::from(r
