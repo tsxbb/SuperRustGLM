f85d07fc58d7b2ff0a13a560ba6c919c4e89af34
tsxbb <142214302+tsxbb@users.noreply.github.com>
2023-11-01 12:25:13 +0000

    Update search.py

--- src/sse_invoke_method/sse_invoke.rs
+++ src/sse_invoke_method/sse_invoke.rs
@@ -13,4 +13,8 @@ use serde_json::{json, Value};
 use futures::stream::StreamExt;
 
 lazy_static::lazy_static! {
-    static ref UNICODE_REGEX: regex::Regex = regex::R
+    static ref UNICODE_REGEX: regex::Regex = regex::Regex::new(r"\\u[0-9a-fA-F]{4}").unwrap();
+}
+
+/*
+ChatGLM-3, ChatGLM-4 Config
