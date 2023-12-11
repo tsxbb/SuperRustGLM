da98fea9409c22ac7d43fc997d1287aefaf843e3
tsxbb <142214302+tsxbb@users.noreply.github.com>
2023-12-11 06:22:39 +0000

    Fix incorrect path

--- src/sse_invoke_method/sse_invoke.rs
+++ src/sse_invoke_method/sse_invoke.rs
@@ -283,4 +283,6 @@ impl SSEInvokeModel {
      */
 
     async fn glm4v_handle_sse_request(user_config: &str, part2_content: String) -> Result<String, Box<dyn Error>> {
-        let json_string = match glm4v_read_config(user_config, "glm-4
+        let json_string = match glm4v_read_config(user_config, "glm-4v").await {
+            Ok(json_string) => json_string,
+           
