6f62aea8cc6de70d346e1e7465ff21c486c2ab95
tsxbb <142214302+tsxbb@users.noreply.github.com>
2023-12-30 10:51:55 +0000

    #54

--- src/sse_invoke_method/sse_invoke.rs
+++ src/sse_invoke_method/sse_invoke.rs
@@ -376,4 +376,7 @@ impl SSEInvokeModel {
                     part1_content = first_part.as_str().to_string();
                 }
                 if let Some(second_part) = captures_message.get(2) {
-                    part2_content = second_part.as
+                    part2_content = second_part.as_str().to_string();
+                }
+            } else {
+                println!
