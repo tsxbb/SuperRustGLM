3e276c0efe6d5d378fc9d69b5aa78b00ac62af51
tsxbb <142214302+tsxbb@users.noreply.github.com>
2024-02-27 07:37:01 +0000

    dm bufio: update last_accessed when relinking a buffer

--- src/sync_invoke_method.rs
+++ src/sync_invoke_method.rs
@@ -3,4 +3,8 @@ mod sync_invoke;
 #[derive(Debug)]
 pub struct ReceiveInvokeModelOnlyText {
     response_sync_message: Option<String>,
-    image
+    image_url: String,
+    default_url: String,
+}
+
+impl ReceiveInvokeModelOnlyTex
